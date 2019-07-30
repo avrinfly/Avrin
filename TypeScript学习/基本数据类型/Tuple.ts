a35146e84aa3d15ef63bed856d1c043ae54a3215
avrin.live.cn@outlook.com <avrin.live.cn@outlook.com>
2019-07-30 15:59:45 +0800

    更新TypeScript学习/基本数据类型-(void,tuple)

---
+++ TypeScript学习/基本数据类型/Tuple.ts
@@ -0,0 +1,10 @@
+//tuple类型允许我们表示具有固定数量的元素的数组，这些元素的类型是已知的，但不必相同。
+//比如我们希望将值表示为一个string和一个number
+//声明元组类型
+let x: [string, number];
+//初始化
+x = [1, 'iven']; // error: initialize it incorrectly
+x = [1];// error: initialize it incorrectly
+
+x = ['iven', 1]; // ok
+alert(x);
--- TypeScript学习/基本数据类型/Void.js
+++ TypeScript学习/基本数据类型/Void.js
@@ -1,10 +1,9 @@
 //void 有些像相反的any：没有任何类型。 你可以认为将此视为无返回值的函数的返回类型 --简而言之void是针对于是函数的类型
-function test(): void {
+function test() {
     console.log('this is a type of void');
 }
 test();
-
 //如果声明的类型是变量，即使将其设为void也是无用的。只能分配undefined和null给它们
 // let variable1: void = 1;//会报错 Error: type "1" cannot be assigned to type "void"
-let variable2: void = undefined;
-let variable3: void = null;
+var variable2 = undefined;
+var variable3 = null;
