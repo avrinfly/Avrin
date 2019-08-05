10ef278f32f7eac45b79eb18d748fc4c5db3cfd5
avrin.live.cn@outlook.com <avrin.live.cn@outlook.com>
2019-08-05 17:06:08 +0800

    更新TypeScript学习/基本数据类型(Type-assertions)

---
+++ TypeScript学习/基本数据类型/Type_assertions.js
@@ -0,0 +1,16 @@
+//type-assertions类型检查（断言）
+//有的时候我们对于某个值的了解会比TS多一些，这通常是因为你知道一个实体具有比它现有类型更确切的类型。
+//类型断言是一种告诉编译器“相信我，我知道我在做什么”的方法。
+//类型断言类似于其他语言中的类型转换，但不执行数据的特殊检查或重新构造。
+//它没有运行时的影响，只在编译阶段起作用，TypeScript 会假设你已经进行了必须的检查
+//断言类型有两种形式：
+//一种是「尖括号」语法
+var someValue = 'this is a string';
+var strLength = someValue.length;
+alert(someValue);
+alert(strLength);
+//另一种是[as]语法
+var moreValue = 'this is another string';
+var strsLength = moreValue.length;
+alert(moreValue);
+alert(strsLength);
