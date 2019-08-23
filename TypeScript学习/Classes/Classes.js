3f73b31896cf8cd7d2f5b3694d0fad28fcdd6217
avrin.live.cn@outlook.com <avrin.live.cn@outlook.com>
2019-08-24 01:11:47 +0800

    更新TypeScript学习/Classes-类的创建

--- TypeScript学习/Classes/Classes.js
+++ TypeScript学习/Classes/Classes.js
@@ -4,12 +4,14 @@
  * @Github: https://github.com/avrinfly
  * @Date: 2019-08-21 00:30:33
  * @LastEditors: hetengfei
- * @LastEditTime: 2019-08-21 01:35:54
+ * @LastEditTime: 2019-08-24 01:10:40
  */
 //在JavaScript中我们使用函数和基于原型的继承来构建可重用的组件，对于我们来说，使用面向对象的方法更加舒服。
 //从ECMAScript 2015(ES6)开始，我们可以使用面向对象的基于类的方法构建我们的应用程序。
 //在TypeScript中，我们也可以使用这些，并把它们编译为适用于所有主流浏览器和平台的JavaScript
 //Classes创建
+//一个类中包含两部分：属性和方法
+//我们声明一个新的类Greeter 有一个greeting属性，一个构造函数，和一个greet方法
 var Greeter = /** @class */ (function () {
     function Greeter(message) {
         this.greeting = message;
@@ -20,4 +22,18 @@ var Greeter = /** @class */ (function () {
     return Greeter;
 }());
 var greeter = new Greeter('world');
-alert(greeter.greet());
+alert(greeter.greet()); //hello world
+//看另外一个例子：
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.alertPerson = function () {
+        console.log(this);
+        return this.name + ':' + this.age;
+    };
+    return Person;
+}());
+var p = new Person('iven', 18);
+alert(p.alertPerson()); //iven:18
--- TypeScript学习/Classes/Classes.ts
+++ TypeScript学习/Classes/Classes.ts
@@ -4,13 +4,15 @@
  * @Github: https://github.com/avrinfly
  * @Date: 2019-08-21 00:30:33
  * @LastEditors: hetengfei
- * @LastEditTime: 2019-08-21 01:35:54
+ * @LastEditTime: 2019-08-24 01:10:40
  */
 //在JavaScript中我们使用函数和基于原型的继承来构建可重用的组件，对于我们来说，使用面向对象的方法更加舒服。
 //从ECMAScript 2015(ES6)开始，我们可以使用面向对象的基于类的方法构建我们的应用程序。
 //在TypeScript中，我们也可以使用这些，并把它们编译为适用于所有主流浏览器和平台的JavaScript
 
 //Classes创建
+//一个类中包含两部分：属性和方法
+//我们声明一个新的类Greeter 有一个greeting属性，一个构造函数，和一个greet方法
 class Greeter {
     greeting: string;
     constructor(message: string) {
@@ -21,4 +23,19 @@ class Greeter {
     }
 }
 let greeter = new Greeter('world');
-alert(greeter.greet());
+alert(greeter.greet());//hello world
+//看另外一个例子：
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    alertPerson() {
+        console.log(this);
+        return this.name + ':' + this.age;
+    }
+}
+let p = new Person('iven', 18);
+alert(p.alertPerson()); //iven:18
